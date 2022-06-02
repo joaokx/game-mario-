686e4b6fb2534176c4bc98ff29a2a28e4406021a
joao pedro <joaopedrobsi10@gmail.com>
2022-06-02 13:49:12 -0300

    primeira versao do jogo

---
+++ js/config.js
@@ -0,0 +1,41 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = ()=>{
+  mario.classList.add('jump');
+
+  setTimeout(() =>{
+      mario.classList.remove('jump');
+  },500);
+}
+
+const loop  = setInterval(() => {
+
+    console.log('loop')
+   const pipePosition = pipe.offsetLeft;
+   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+   
+
+   if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+     
+       pipe.style.amimation = 'none';
+       pipe.style.animation =`${pipePosition}px`;
+
+       mario.style.amimation = 'none';
+       mario.style.animation =`${marioPosition}px`;
+
+       mario.src="./images/game-over.png";
+       mario.style.width = "75px"
+       mario.style.marginleft ="50px"
+
+       clearInterval(loop)
+
+   }else{
+    mario.innerHTML = `game over` 
+   }
+
+
+}, 10 )
+
+
+
